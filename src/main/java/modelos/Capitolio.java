03dab7a38f70af5e84932caa459552e51d95d09b
JordanMedina99 <jordanmedina0060@gmail.com>
2023-05-24 12:10:37 -0600

    se modifico todo

--- src/main/java/modelos/Capitolio.java
+++ src/main/java/modelos/Capitolio.java
@@ -4,80 +4,89 @@ import java.io.Serializable;
 
 public class Capitolio implements Serializable {
     private static final long serialVersionUID = 1L;
-    private int idCapitolio;
-    private String nombrePresidente;
-    private int numeroHabitantes;
+    private int id_capitolio;
+    private String nombre_presidente;
+    private int numero_habitantes;
     private String ubicacion;
     private String clima;
-    private float porcentajeHombres;
-    private float porcentajeMujeres;
-    private String lugaresInteres;
+    private float porcentaje_hombres;
+    private float porcentaje_mujeres;
+    private String lugares_interes;
 
-    public Capitolio() {
+    public Capitolio(int id_capitolio, String nombre_presidente, int numero_habitantes, String ubicacion, String clima, float porcentaje_hombres, float porcentaje_mujeres, String lugares_interes){
+    	 this.id_capitolio = id_capitolio;
+    	 this.nombre_presidente = nombre_presidente;
+    	 this.numero_habitantes = numero_habitantes;
+    	 this.ubicacion = ubicacion;
+    	 this.clima = clima;
+    	 this.porcentaje_hombres = porcentaje_hombres;
+    	 this.porcentaje_mujeres = porcentaje_mujeres;
+    	 this.lugares_interes = lugares_interes;
+    	 
     }
 
-    public int getIdCapitolio() {
-        return idCapitolio;
+    public int getid_capitolio() {
+        return id_capitolio;
     }
 
-    public void setIdCapitolio(int idCapitolio) {
-        this.idCapitolio = idCapitolio;
+    public void setid_capitolio(int id_capitolio) {
+        this.id_capitolio = id_capitolio;
     }
 
-    public String getNombrePresidente() {
-        return nombrePresidente;
+    public String getnombre_presidente() {
+        return nombre_presidente;
     }
 
-    public void setNombrePresidente(String nombrePresidente) {
-        this.nombrePresidente = nombrePresidente;
+    public void setnombre_presidente(String nombre_presidente) {
+        this.nombre_presidente = nombre_presidente;
     }
 
-    public int getNumeroHabitantes() {
-        return numeroHabitantes;
+    public int getnumero_habitantes() {
+        return numero_habitantes;
     }
 
-    public void setNumeroHabitantes(int numeroHabitantes) {
-        this.numeroHabitantes = numeroHabitantes;
+    public void setnumero_habitantes(int numero_habitantes) {
+        this.numero_habitantes = numero_habitantes;
     }
 
-    public String getUbicacion() {
+    public String getubicacion() {
         return ubicacion;
     }
 
-    public void setUbicacion(String ubicacion) {
+    public void setubicacion(String ubicacion) {
         this.ubicacion = ubicacion;
     }
 
-    public String getClima() {
+    public String getclima() {
         return clima;
     }
 
-    public void setClima(String clima) {
+    public void setclima(String clima) {
         this.clima = clima;
     }
 
-    public float getPorcentajeHombres() {
-        return porcentajeHombres;
+    public float getporcentaje_hombres() {
+        return porcentaje_hombres;
     }
 
-    public void setPorcentajeHombres(float porcentajeHombres) {
-        this.porcentajeHombres = porcentajeHombres;
+    public void setporcentaje_hombres(float porcentaje_hombres) {
+        this.porcentaje_hombres = porcentaje_hombres;
     }
 
-    public float getPorcentajeMujeres() {
-        return porcentajeMujeres;
+    public float getporcentaje_mujeres() {
+        return porcentaje_mujeres;
     }
 
-    public void setPorcentajeMujeres(float porcentajeMujeres) {
-        this.porcentajeMujeres = porcentajeMujeres;
+    public void setporcentaje_mujeres(float porcentaje_mujeres) {
+        this.porcentaje_mujeres = porcentaje_mujeres;
     }
 
-    public String getLugaresInteres() {
-        return lugaresInteres;
+    public String getlugares_interes() {
+        return lugares_interes;
     }
 
-    public void setLugaresInteres(String lugaresInteres) {
-        this.lugaresInteres = lugaresInteres;
+    public void setlugares_interes(String lugares_interes) {
+        this.lugares_interes = lugares_interes;
     }
 }
 
