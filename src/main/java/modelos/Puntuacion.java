cbdc77fb261536e06069918805ea937da478879f
JordanMedina99 <jordanmedina0060@gmail.com>
2023-05-24 13:27:10 -0600

    se modifico todo

--- src/main/java/modelos/Puntuacion.java
+++ src/main/java/modelos/Puntuacion.java
@@ -4,44 +4,48 @@ import java.io.Serializable;
 
 public class Puntuacion implements Serializable {
     private static final long serialVersionUID = 1L;
-    private int idPuntuacion;
+    private int id_puntuacion;
     private int valor;
-    private int tributoId;
-    private int pruebaId;
+    private int tributo_id;
+    private int prueba_id;
 
-    public Puntuacion() {
+    public Puntuacion(int id_puntuacion, int valor, int tributo_id, int prueba_id){
+    	this.id_puntuacion = id_puntuacion;
+        this.valor = valor;
+        this.tributo_id = tributo_id;
+        this.prueba_id = prueba_id;
     }
 
-    public int getIdPuntuacion() {
-        return idPuntuacion;
+    public int getid_puntuacion() {
+        return id_puntuacion;
     }
 
-    public void setIdPuntuacion(int idPuntuacion) {
-        this.idPuntuacion = idPuntuacion;
+    public void setid_puntuacion(int id_puntuacion) {
+        this.id_puntuacion = id_puntuacion;
     }
 
-    public int getValor() {
+    public int getvalor() {
         return valor;
     }
 
-    public void setValor(int valor) {
+    public void setvalor(int valor) {
         this.valor = valor;
     }
 
-    public int getTributoId() {
-        return tributoId;
+    public int gettributo_id() {
+        return tributo_id;
     }
 
-    public void setTributoId(int tributoId) {
-        this.tributoId = tributoId;
+    public void settributo_id(int tributo_id) {
+        this.tributo_id = tributo_id;
     }
 
-    public int getPruebaId() {
-        return pruebaId;
+    public int getprueba_id() {
+        return prueba_id;
     }
 
-    public void setPruebaId(int pruebaId) {
-        this.pruebaId = pruebaId;
+    public void setprueba_id(int prueba_id) {
+        this.prueba_id = prueba_id;
     }
 }
 
