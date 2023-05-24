5dbc715529d8c9445fca98a6d6e615b1f43336b8
JordanMedina99 <jordanmedina0060@gmail.com>
2023-05-24 13:18:11 -0600

    se modifico todo

--- src/main/java/modelos/Prueba.java
+++ src/main/java/modelos/Prueba.java
@@ -6,70 +6,77 @@ import java.sql.Time;
 
 public class Prueba implements Serializable {
     private static final long serialVersionUID = 1L;
-    private int idPrueba;
+    private int id_prueba;
     private String codigo;
     private String nombre;
     private String tipo;
-    private String dificultadId;
-    private int vencedorId;
-    private Time tiempoEmpleado;
+    private String dificultad_id;
+    private int vencedor_id;
+    private Time tiempo_empleado;
 
-    public Prueba() {
+    public Prueba(int id_prueba, String codigo, String nombre, String tipo, String dificultad_id, int vencedor_id, Time tiempo_empleado){
+    	this.id_prueba = id_prueba;
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.dificultad_id = dificultad_id;
+        this.vencedor_id = vencedor_id;
+        this.tiempo_empleado = tiempo_empleado;
     }
 
-    public int getIdPrueba() {
-        return idPrueba;
+    public int getid_prueba() {
+        return id_prueba;
     }
 
-    public void setIdPrueba(int idPrueba) {
-        this.idPrueba = idPrueba;
+    public void setid_prueba(int id_prueba) {
+        this.id_prueba = id_prueba;
     }
 
-    public String getCodigo() {
+    public String getcodigo() {
         return codigo;
     }
 
-    public void setCodigo(String codigo) {
+    public void setcodigo(String codigo) {
         this.codigo = codigo;
     }
 
-    public String getNombre() {
+    public String getnombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    public void setnombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public String getTipo() {
+    public String gettipo() {
         return tipo;
     }
 
-    public void setTipo(String tipo) {
+    public void settipo(String tipo) {
         this.tipo = tipo;
     }
 
-    public String getDificultadId() {
-        return dificultadId;
+    public String getdificultad_id() {
+        return dificultad_id;
     }
 
-    public void setDificultadId(String dificultadId) {
-        this.dificultadId = dificultadId;
+    public void setdificultad_id(String dificultad_id) {
+        this.dificultad_id = dificultad_id;
     }
 
-    public int getVencedorId() {
-        return vencedorId;
+    public int getvencedor_id() {
+        return vencedor_id;
     }
 
-    public void setVencedorId(int vencedorId) {
-        this.vencedorId = vencedorId;
+    public void setvencedor_id(int vencedor_id) {
+        this.vencedor_id = vencedor_id;
     }
 
-    public Time getTiempoEmpleado() {
-        return tiempoEmpleado;
+    public Time gettiempo_empleado() {
+        return tiempo_empleado;
     }
 
-    public void setTiempoEmpleado(Time tiempoEmpleado) {
-        this.tiempoEmpleado = tiempoEmpleado;
+    public void settiempo_empleado(Time tiempo_empleado) {
+        this.tiempo_empleado = tiempo_empleado;
     }
 }
