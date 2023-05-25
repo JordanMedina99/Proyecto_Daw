488bf43c76b073b436fd00baff6b91ad21e0b3ce
JordanMedina99 <jordanmedina0060@gmail.com>
2023-05-24 22:04:06 -0600

    peticion a tabla distritos para mostrar datos

--- src/main/java/conexion/Conexion.java
+++ src/main/java/conexion/Conexion.java
@@ -4,6 +4,7 @@ import java.sql.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Conexion {
