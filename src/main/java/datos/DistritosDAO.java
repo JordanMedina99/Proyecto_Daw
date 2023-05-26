c34f178ae69a59ef05702e6d3df41b1367b145ef
JordanMedina99 <jordanmedina0060@gmail.com>
2023-05-26 14:13:32 -0600

    creacion y modificaciones

--- src/main/java/datos/DistritosDAO.java
+++ src/main/java/datos/DistritosDAO.java
@@ -49,6 +49,79 @@ public class DistritosDAO {
 
 	    return lista;
 	}
+	
+	 public void insertarDistrito(Distrito distrito) throws ClassNotFoundException {
+	        Connection conn = null;
+	        PreparedStatement ps = null;
+
+	        try {
+	            conn = Conexion.getConnection();
+	            ps = conn.prepareStatement("INSERT INTO distrito (nombre, especializacion, puestos_trabajo, juegos_ganados, cantidad_habitantes, ubicacion, clima, porcentaje_hombres, porcentaje_mujeres, lider_nombre) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+	            ps.setString(1, distrito.getnombre());
+	            ps.setString(2, distrito.getespecializacion());
+	            ps.setInt(3, distrito.getpuestos_trabajo());
+	            ps.setInt(4, distrito.getjuegos_ganados());
+	            ps.setInt(5, distrito.getcantidad_habitantes());
+	            ps.setString(6, distrito.getubicacion());
+	            ps.setString(7, distrito.getclima());
+	            ps.setFloat(8, distrito.getporcentaje_hombres());
+	            ps.setFloat(9, distrito.getporcentaje_mujeres());
+	            ps.setString(10, distrito.getlider_nombre());
+	            
+	            ps.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        } finally {
+	            Conexion.close(ps);
+	            Conexion.close(conn);
+	        }
+	    }
+
+	    public void actualizarDistrito(Distrito distrito) throws ClassNotFoundException {
+	        Connection conn = null;
+	        PreparedStatement ps = null;
+
+	        try {
+	            conn = Conexion.getConnection();
+	            ps = conn.prepareStatement("UPDATE distrito SET nombre=?, especializacion=?, puestos_trabajo=?, juegos_ganados=?, cantidad_habitantes=?, ubicacion=?, clima=?, porcentaje_hombres=?, porcentaje_mujeres=?, lider_nombre=? WHERE id_distrito=?");
+	            ps.setString(1, distrito.getnombre());
+	            ps.setString(2, distrito.getespecializacion());
+	            ps.setInt(3, distrito.getpuestos_trabajo());
+	            ps.setInt(4, distrito.getjuegos_ganados());
+	            ps.setInt(5, distrito.getcantidad_habitantes());
+	            ps.setString(6, distrito.getubicacion());
+	            ps.setString(7, distrito.getclima());
+	            ps.setFloat(8, distrito.getporcentaje_hombres());
+	            ps.setFloat(9, distrito.getporcentaje_mujeres());
+	            ps.setString(10, distrito.getlider_nombre());
+	            ps.setInt(11, distrito.getid_distrito());
+	            
+	            ps.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        } finally {
+	            Conexion.close(ps);
+	            Conexion.close(conn);
+	        }
+	    }
+
+	    public void eliminarDistrito(int idDistrito) throws ClassNotFoundException {
+	        Connection conn = null;
+	        PreparedStatement ps = null;
+
+	        try {
+	            conn = Conexion.getConnection();
+	            ps = conn.prepareStatement("DELETE FROM distrito WHERE id_distrito=?");
+	            ps.setInt(1, idDistrito);
+	            
+	            ps.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        } finally {
+	            Conexion.close(ps);
+	            Conexion.close(conn);
+	        }
+	    }
 
 }
 
